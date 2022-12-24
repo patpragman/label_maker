8afe632eeadfe8444d2014485e80fe79e03aece4
Patrick Pragman <patrickpragman@pop-os.localdomain>
2022-12-24 14:30:10 -0900

    added save states from session to session

--- app.py
+++ app.py
@@ -19,11 +19,14 @@ from panes.image_pane import ImagePane
 from panes.list_pane import ListPane
 from data_model.imagescores import Folder, ScoredImage
 
+from tkinter import filedialog
+
 
 class Application:
 
     def __init__(self):
 
+        # save state (last file worked on) code lives here
         self.save_state_file = path.join(getcwd(), "savestates/save.json")
         if path.exists(self.save_state_file):
             with open(self.save_state_file, "r") as save_state_file:
@@ -44,20 +47,41 @@ class Application:
         # set up the sub panes
         self.image_pane = ImagePane(self.root)
         self.control_pane = ControlPane(self.root)
-        self.list_pane = ListPane(self.root)
+        self.list_pane = ListPane(self.root,
+                                  choose_folder_callback=self._choose_folder)
 
         # set up the layout of the widgets in the application in the grid
         self.image_pane.grid(row=0, column=0)
         self.control_pane.grid(row=1, column=0)
         self.list_pane.grid(row=0, column=1)
 
+        # we need to register these states so they save when you close out of the program
+        self._register_save_states()
+
+    def _register_save_states(self):
+        # unregister previous state
+        atexit.unregister(self.save)
+        atexit.register(self.current_folder.save)
+
+        # register new states
         atexit.register(self.save)
         atexit.register(self.current_folder.save)
 
+    def _choose_folder(self):
+        # first save the folder you're currently in
+        self.current_folder.save()
+
+        # now choose a new folder
+        self.current_folder = Folder(filedialog.askdirectory(title="choose the directory you want to explore"))
+
+        # re-register the save states
+        self._register_save_states()
+
+
     def save(self):
+        # open up the save state file, and save the current state
         with open(self.save_state_file, "w") as save_state_file:
             dump(str(self.current_folder), save_state_file)
-
 
     def run(self):
         # start the application
